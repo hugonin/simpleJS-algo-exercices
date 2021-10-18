f16d0ae7265c92caf95a2ee68c41c552668dd982
Hugonin <jeanhugues.doumbe@gmail.com>
2021-10-18 19:59:41 +0200

    Added the first part of exercises files

---
+++ exo5.js
@@ -0,0 +1,6 @@
+/*
+ * Ecrire un programme pour un professeur flemmard.​
+ * On dispose d'une liste de notes comprises entre 0 et 20​
+ * Ecrire un commentaire pour chacune de ces copies d'élève
+ */
+
