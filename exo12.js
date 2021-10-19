e0bb2ef666b0a2798a07ed11503bef2c61666263
Hugonin <jeanhugues.doumbe@gmail.com>
2021-10-19 15:33:48 +0200

    added solutions for exercises 8 and 12

--- exo12.js
+++ exo12.js
@@ -4,3 +4,23 @@
 * Si une température négative est aussi proche de 0 qu'une température positive, la valeur négative prend le dessus.​
 ​*
 */
+
+const temperatures = [
+  { degres: 12, estProcheDeZero: false },
+  { degres: 25, estProcheDeZero: false },
+  { degres: 5, estProcheDeZero: true },
+  { degres: 7, estProcheDeZero: true },
+  { degres: 6, estProcheDeZero: true },
+  { degres: -5, estProcheDeZero: true },
+];
+
+
+temperatures.forEach((temperature) => {
+  if (temperature.estProcheDeZero) {
+    console.log(
+      `La temperature la plus proche de zéro est ${temperature.degres}°C`
+    );
+  }
+});
+
+
