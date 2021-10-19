4af1e354bb3ada4f852a278c311135267adbeeba
Hugonin <jeanhugues.doumbe@gmail.com>
2021-10-19 15:47:03 +0200

    added solution for exercise 13

--- exo13.js
+++ exo13.js
@@ -2,3 +2,12 @@
 * Vous disposez d'une liste de nombre.​
 * Triez la liste du plus petit au plus grand
 */
+
+const listeNombres = [12,25,5,7,6,-5]
+
+function compareNombres(n1,n2) {
+    return n1 - n2
+}
+
+console.log("Liste triée: " + listeNombres.sort(compareNombres))
+
